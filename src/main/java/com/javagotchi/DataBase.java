199372e9809a11ff61c5b855e6fc3b33fa1f8593
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-11-29 12:44:31 +0100

    add database constants

--- src/main/java/com/javagotchi/DataBase.java
+++ src/main/java/com/javagotchi/DataBase.java
@@ -13,6 +13,23 @@ public class DataBase {
     private Connection connection;
     public static final String DB_NAME = "database.db";
     public static final String CONNECTION_STRING = "jdbc:sqlite:" + DB_NAME;
+    public static final String TABLE_CHARACTER = "character";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_HAPPINESS = "happiness";
+    public static final String COLUMN_CLEANLINESS = "cleanliness";
+    public static final String COLUMN_WEIGHT = "weight";
+    public static final String COLUMN_ENERGY = "energy";
+    public static final String COLUMN_LEVEL = "level";
+    public static final String COLUMN_AGE = "age";
+    public static final String COLUMN_LAST_USAGE_TIME = "last_usage_time";
+    public static final int INDEX_CHARACTER_ID = 1;
+    public static final int INDEX_CHARACTER_HAPPINESS = 2;
+    public static final int INDEX_CHARACTER_CLEANLINESS = 3;
+    public static final int INDEX_CHARACTER_WEIGHT = 4;
+    public static final int INDEX_CHARACTER_ENERGY = 5;
+    public static final int INDEX_CHARACTER_LEVEL = 6;
+    public static final int INDEX_CHARACTER_AGE = 7;
+    public static final int INDEX_CHARACTER_LAST_USAGE_TIME = 8;
 
     /*
     * Opens a database connection
