268508955531e4403f07fe054872892cb791aeaf
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-02 00:19:46 +0100

    add private fields getters setters and noargs constructor using lombok

--- src/main/java/com/javagotchi/Character.java
+++ src/main/java/com/javagotchi/Character.java
@@ -1,4 +1,21 @@
 package com.javagotchi;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
 public class Character {
+
+    private int hunger;
+    private int cleanliness;
+    private int weight;
+    private int energy;
+    private int level;
+    private int age;
+    private int happiness;
+
+    
 }
