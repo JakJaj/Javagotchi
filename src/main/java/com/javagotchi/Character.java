1c3448e91209b4572d7be705b542388d4655ce96
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-02 00:42:28 +0100

    add wakeUp method

--- src/main/java/com/javagotchi/Character.java
+++ src/main/java/com/javagotchi/Character.java
@@ -3,6 +3,8 @@ package com.javagotchi;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.Duration;
 import java.time.LocalTime;
 
 @Getter
@@ -32,6 +34,22 @@ public class Character {
     public LocalTime sleep(){
         this.sleeping = true;
         return LocalTime.now();
+    }
+    /*
+    * Waking up a character from a sleep, and making character's go up according to the amount of minutes that elapsed from character going to sleep.
+    * Setting a sleeping field to false
+    * @params       LocalTime of a moment when a character went to sleep
+     */
+    public void wakeUp(LocalTime sleepTime) {
+        LocalTime now = LocalTime.now();
+        Duration timeElapsed = Duration.between(sleepTime, now);
+        long sleepingTime = timeElapsed.toMinutes();
+        if (this.energy + sleepingTime > 100) {
+            this.energy = 100;
+        } else {
+            this.energy = this.energy + (int) sleepingTime;
+        }
+        sleeping = false;
     }
     public void play(){
 
