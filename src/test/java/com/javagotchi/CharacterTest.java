de33642a2098c0b1a93035d29e86168d58c0751d
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-05 21:54:58 +0100

    add empty test methods

---
+++ src/test/java/com/javagotchi/CharacterTest.java
@@ -0,0 +1,32 @@
+package com.javagotchi;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CharacterTest {
+
+    @Test
+    void eat() {
+    }
+
+    @Test
+    void clean() {
+    }
+
+    @Test
+    void sleep() {
+    }
+
+    @Test
+    void wakeUp() {
+    }
+
+    @Test
+    void play() {
+    }
+
+    @Test
+    void levelUp() {
+    }
+}
