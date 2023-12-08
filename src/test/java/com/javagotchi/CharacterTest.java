e60d102ee4dc663b635d6d4bfae81477a60da67a
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-08 17:09:08 +0100

    add test for test method

--- src/test/java/com/javagotchi/CharacterTest.java
+++ src/test/java/com/javagotchi/CharacterTest.java
@@ -16,7 +16,7 @@ class CharacterTest {
         testCharacter = new Character();
         testCharacter.setHunger(50);
         testCharacter.setCleanliness(50);
-        testCharacter.setWeight(10);
+        testCharacter.setWeight(50);
         testCharacter.setEnergy(50);
         testCharacter.setHealth(50);
         testCharacter.setLevel(10);
@@ -31,17 +31,17 @@ class CharacterTest {
         assertEquals(75,testCharacter.getHunger());
         assertEquals(55,testCharacter.getHappiness());
         assertEquals(54,testCharacter.getExperience());
-        assertEquals(10,testCharacter.getWeight());
+        assertEquals(50,testCharacter.getWeight());
         testCharacter.eat();
         assertEquals(100,testCharacter.getHunger());
         assertEquals(60,testCharacter.getHappiness());
         assertEquals(58,testCharacter.getExperience());
-        assertEquals(10,testCharacter.getWeight());
+        assertEquals(50,testCharacter.getWeight());
         testCharacter.eat();
         assertEquals(100,testCharacter.getHunger());
         assertEquals(50,testCharacter.getHappiness());
         assertEquals(58,testCharacter.getExperience());
-        assertEquals(11,testCharacter.getWeight());
+        assertEquals(51,testCharacter.getWeight());
     }
 
     @Test
@@ -77,6 +77,38 @@ class CharacterTest {
 
     @Test
     void play() {
+        testCharacter.play();
+        assertEquals(60,testCharacter.getHappiness());
+        assertEquals(55,testCharacter.getExperience());
+        assertEquals(35,testCharacter.getEnergy());
+        assertEquals(40,testCharacter.getHunger());
+        assertEquals(40,testCharacter.getCleanliness());
+        assertEquals(51,testCharacter.getHealth());
+        assertEquals(50,testCharacter.getWeight());
+        testCharacter.play();
+        assertEquals(70,testCharacter.getHappiness());
+        assertEquals(60,testCharacter.getExperience());
+        assertEquals(20,testCharacter.getEnergy());
+        assertEquals(30,testCharacter.getHunger());
+        assertEquals(30,testCharacter.getCleanliness());
+        assertEquals(52,testCharacter.getHealth());
+        assertEquals(50,testCharacter.getWeight());
+        testCharacter.play();
+        assertEquals(80,testCharacter.getHappiness());
+        assertEquals(65,testCharacter.getExperience());
+        assertEquals(5,testCharacter.getEnergy());
+        assertEquals(20,testCharacter.getHunger());
+        assertEquals(20,testCharacter.getCleanliness());
+        assertEquals(53,testCharacter.getHealth());
+        assertEquals(50,testCharacter.getWeight());
+        testCharacter.play();
+        assertEquals(70,testCharacter.getHappiness());
+        assertEquals(65,testCharacter.getExperience());
+        assertEquals(0,testCharacter.getEnergy());
+        assertEquals(10,testCharacter.getHunger());
+        assertEquals(10,testCharacter.getCleanliness());
+        assertEquals(43,testCharacter.getHealth());
+        assertEquals(49,testCharacter.getWeight());
     }
 
     @Test
