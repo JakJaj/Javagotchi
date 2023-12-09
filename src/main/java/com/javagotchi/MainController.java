820e1ebc23de652f0b26f5e1f14e4e1c7267f701
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-09 14:07:08 +0100

    add initialize method to controller

--- src/main/java/com/javagotchi/MainController.java
+++ src/main/java/com/javagotchi/MainController.java
@@ -1,4 +1,14 @@
 package com.javagotchi;
 
+import javafx.fxml.FXML;
+
 public class MainController {
+    @FXML
+    public void initialize(){
+        DataBase dataBase = new DataBase();
+        if (dataBase.open()){
+            Character character = dataBase.getLatestCharacterData();
+        }
+        dataBase.close();
+    }
 }
