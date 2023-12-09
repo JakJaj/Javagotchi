8ea1ef44ef636937f03deb60068d99fc6ff30a0a
Jakub Jajonek <jakub.jajonek@gmail.com>
2023-12-09 14:09:17 +0100

    add save method to controller

--- src/main/java/com/javagotchi/MainController.java
+++ src/main/java/com/javagotchi/MainController.java
@@ -3,11 +3,21 @@ package com.javagotchi;
 import javafx.fxml.FXML;
 
 public class MainController {
+    DataBase dataBase = new DataBase();
+    Character character;
     @FXML
-    public void initialize(){
-        DataBase dataBase = new DataBase();
-        if (dataBase.open()){
-            Character character = dataBase.getLatestCharacterData();
+    public void initialize() {
+
+        if (dataBase.open()) {
+            character = dataBase.getLatestCharacterData();
+        }
+        dataBase.close();
+    }
+
+    @FXML
+    public void save() {
+        if(dataBase.open()){
+            dataBase.insertNewestData(character);
         }
         dataBase.close();
     }
