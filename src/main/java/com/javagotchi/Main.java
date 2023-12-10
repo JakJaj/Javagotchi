402392e6c8c4b8c6e3bb8af4ac13f9cadc62803f
gabgab1337 <gabrielhanszke@gmail.com>
2023-12-10 16:12:03 +0100

    Refactor UI and add character stats with labels

--- src/main/java/com/javagotchi/Main.java
+++ src/main/java/com/javagotchi/Main.java
@@ -10,13 +10,27 @@ import javafx.scene.layout.*;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.geometry.*; // pos only prob?
+import javafx.util.Duration;
 import java.io.IOException;
 import java.io.InputStream;
-import javafx.util.Duration;
+import java.time.LocalTime;
+
 
 public class Main extends Application {
+    private Character character;
+    private Label nameLabel;
+    private Label ageLabel;
+    private Label healthLabel;
+    private Label hungerLabel;
+    private Label cleanlinessLabel;
+    private Label happinessLabel;
+    private Label energyLabel;
+    private LocalTime bedtime;
+
     @Override
     public void start(Stage stage) throws IOException {
+        character = new Character();
+
         FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("main.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 800, 600);
 
@@ -24,6 +38,41 @@ public class Main extends Application {
         Pane topSection = new Pane();
         topSection.setPrefSize(800, 150);
         topSection.setStyle(containerBackground);
+
+        // Test labels
+        nameLabel = new Label("Energy: " + character.getEnergy());
+        ageLabel = new Label("Age: " + character.getAge());
+        healthLabel = new Label("Health: " + character.getHealth());
+        hungerLabel = new Label("Hunger: " + character.getHunger());
+        cleanlinessLabel = new Label("Cleanliness: " + character.getCleanliness());
+        happinessLabel = new Label("Happiness: " + character.getHappiness());
+        energyLabel = new Label("Energy: " + character.getEnergy());
+
+        String labelStyle = "-fx-text-fill: #f2f2f2; -fx-font-family: 'Helvetica';";
+        nameLabel.setLayoutX(20);
+        nameLabel.setLayoutY(20);
+        nameLabel.setStyle(labelStyle);
+        ageLabel.setLayoutX(20);
+        ageLabel.setLayoutY(50);
+        ageLabel.setStyle(labelStyle);
+        healthLabel.setLayoutX(20);
+        healthLabel.setLayoutY(80);
+        healthLabel.setStyle(labelStyle);
+        hungerLabel.setLayoutX(20);
+        hungerLabel.setLayoutY(110);
+        hungerLabel.setStyle(labelStyle);
+        cleanlinessLabel.setLayoutX(150);
+        cleanlinessLabel.setLayoutY(20);
+        cleanlinessLabel.setStyle(labelStyle);
+        happinessLabel.setLayoutX(150);
+        happinessLabel.setLayoutY(50);
+        happinessLabel.setStyle(labelStyle);
+        energyLabel.setLayoutX(150);
+        energyLabel.setLayoutY(80);
+        energyLabel.setStyle(labelStyle);
+
+        topSection.getChildren().addAll(nameLabel, ageLabel, healthLabel, hungerLabel, cleanlinessLabel, happinessLabel, energyLabel);
+        // Top section needs some cleaning up
 
         Pane bottomSection = new Pane();
         bottomSection.setPrefSize(800, 150);
@@ -35,7 +84,7 @@ public class Main extends Application {
         // Load image as background
         ClassLoader classloader = Thread.currentThread().getContextClassLoader();
         InputStream is = classloader.getResourceAsStream("background.jpg");
-        if (is == null) {
+        if (is == null) { 
             System.out.println("Error: image not found");
             System.exit(1);
         }
@@ -51,49 +100,88 @@ public class Main extends Application {
         VBox root = new VBox(topSection, centerSection, bottomSection);
         scene.setRoot(root);
 
-        Button button1 = new Button("EAT");
-        Button button2 = new Button("PLAY");
-        Button button3 = new Button("CLEAN");
-        Button button4 = new Button("SLEEP");
+        Button buttonEat = new Button("EAT");
+        Button buttonPlay = new Button("PLAY");
+        Button buttonClean = new Button("CLEAN");
+        Button buttonSleep = new Button();
+        // I'm not sure about that one
+        if (character.isSleeping()){
+            buttonSleep.setText("SLEEP");}
+        else{
+            buttonSleep.setText("WAKE UP");}
 
-        button1.setPrefSize(120, 60);
-        button2.setPrefSize(120, 60);
-        button3.setPrefSize(120, 60);
-        button4.setPrefSize(120, 60);
 
-        String buttonStyle = "-fx-background-color: #2a232e; -fx-text-fill: #f2f2f2; -fx-font-family: 'Helvetica';"; // it should go to fxml file
-        button1.setStyle(buttonStyle);
-        button2.setStyle(buttonStyle);
-        button3.setStyle(buttonStyle);
-        button4.setStyle(buttonStyle);
+        buttonEat.setPrefSize(120, 60);
+        buttonPlay.setPrefSize(120, 60);
+        buttonClean.setPrefSize(120, 60);
+        buttonSleep.setPrefSize(120, 60);
+
+        String buttonStyle = "-fx-background-color: #2a232e; -fx-text-fill: #f2f2f2; -fx-font-family: 'Helvetica';"; // it should go into fxml file
+        buttonEat.setStyle(buttonStyle);
+        buttonPlay.setStyle(buttonStyle);
+        buttonClean.setStyle(buttonStyle);
+        buttonSleep.setStyle(buttonStyle);
 
         // Button press event handlers
-        button1.setOnAction(e -> System.out.println("EAT"));
-        button2.setOnAction(e -> System.out.println("PLAY"));
-        button3.setOnAction(e -> System.out.println("CLEAN"));
-        button4.setOnAction(e -> System.out.println("SLEEP"));
+        buttonEat.setOnAction(e -> {
+            character.eat();
+            updateLabels();
+            System.out.println("EAT");
+        });
+        buttonPlay.setOnAction(e -> {
+            character.play();
+            updateLabels();
+            System.out.println("PLAY");
+        });
+        buttonClean.setOnAction(e -> {
+            character.clean();
+            updateLabels();
+            System.out.println("CLEAN");
+        });
+        buttonSleep.setOnAction(e -> {
+            if (character.isSleeping()){
+                character.wakeUp(bedtime);
+                buttonSleep.setText("SLEEP");
+                System.out.println("THEY WOKE UP");}
+            else{
+                bedtime = character.sleep();
+                buttonSleep.setText("WAKE UP");
+                System.out.println("SLEEPY EPPY :3");}
+            updateLabels();
+        });
 
-        HBox buttonContainer = new HBox(button1, button2, button3, button4);
+        HBox buttonContainer = new HBox(buttonEat, buttonPlay, buttonClean, buttonSleep);
         buttonContainer.setSpacing(30);
         buttonContainer.setAlignment(Pos.CENTER);
 
-        double totalButtonWidth = (button1.getPrefWidth() + button2.getPrefWidth() + button3.getPrefWidth() + button4.getPrefWidth()) + (buttonContainer.getSpacing() * 3); // padding calc
+        double totalButtonWidth = (buttonEat.getPrefWidth() + buttonPlay.getPrefWidth() + buttonClean.getPrefWidth() + buttonSleep.getPrefWidth()) + (buttonContainer.getSpacing() * 3); // padding calc
         double padding = (800 - totalButtonWidth) / 2;
         buttonContainer.setPadding(new Insets(0, padding, 0, padding));
         buttonContainer.layoutYProperty().bind(bottomSection.heightProperty().subtract(buttonContainer.heightProperty()).divide(2));
-        bottomSection.getChildren().add(buttonContainer);        
+        bottomSection.getChildren().add(buttonContainer);
 
         stage.setTitle("Welcome to Javagotchi!");
         stage.setScene(scene);
         stage.setResizable(false); // Block window resizing from users
         stage.show();
 
-        
+
         Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(10), event -> {
-            stage.setScene(scene); // Refreshing scene every 10 seconds (mainly for top bars)
+            stage.setScene(scene);
+            updateLabels(); // Refreshing scene every 10 seconds (mainly for top bars)
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
+    }
+
+    private void updateLabels() {
+        nameLabel.setText("Energy: " + character.getEnergy());
+        ageLabel.setText("Age: " + character.getAge());
+        healthLabel.setText("Health: " + character.getHealth());
+        hungerLabel.setText("Hunger: " + character.getHunger());
+        cleanlinessLabel.setText("Cleanliness: " + character.getCleanliness());
+        happinessLabel.setText("Happiness: " + character.getHappiness());
+        energyLabel.setText("Energy: " + character.getEnergy());
     }
 
     public static void main(String[] args) {
