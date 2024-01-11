b336667cdc543a3e13ac13ee2cfdb0234d2cc47a
Jakub Jajonek <jakub.jajonek@gmail.com>
2024-01-11 20:08:10 +0100

    add game over window

--- src/main/java/com/javagotchi/BrickBreakerGame.java
+++ src/main/java/com/javagotchi/BrickBreakerGame.java
@@ -3,12 +3,17 @@ package com.javagotchi;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.input.KeyCode;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -25,7 +30,8 @@ public class BrickBreakerGame extends Application {
     private Circle ball;
     private double ballSpeedX = -1.5;
     private double ballSpeedY = -1.5;
-    
+    private boolean gameOver = false;
+    Timeline timeline;
     public static void main(String[] args) {
         launch(args);
     }
@@ -41,22 +47,28 @@ public class BrickBreakerGame extends Application {
         createBricks();
 
         scene.setOnKeyPressed(e -> {
-            if (e.getCode() == KeyCode.LEFT) {
-                movePaddleLeft();
-            } else if (e.getCode() == KeyCode.RIGHT) {
-                movePaddleRight();
+            if(!gameOver) {
+                if (e.getCode() == KeyCode.LEFT) {
+                    movePaddleLeft();
+                } else if (e.getCode() == KeyCode.RIGHT) {
+                    movePaddleRight();
+                }
             }
         });
 
-        Timeline timeline = new Timeline(new KeyFrame(Duration.millis(10), event -> {
-            moveBall();
-            checkCollision();
+        timeline = new Timeline(new KeyFrame(Duration.millis(10), event -> {
+            if(!gameOver) {
+                moveBall();
+                checkCollision();
+            }
         }));
         timeline.setCycleCount(Timeline.INDEFINITE);
         timeline.play();
 
         primaryStage.setScene(scene);
         primaryStage.show();
+
+        checkGameOver(primaryStage);
     }
 
     private void createPaddle() {
@@ -101,6 +113,7 @@ public class BrickBreakerGame extends Application {
             ballSpeedY = 0;
             ballSpeedX = 0;
             System.out.println("Game Over");
+            gameOver = true;
 
         }
     }
@@ -110,5 +123,56 @@ public class BrickBreakerGame extends Application {
             ballSpeedY *= -1;
         }
         // Add collision detection with bricks here
+    }
+    private void checkGameOver(Stage primaryStage) {
+        Timeline gameOverTimeline = new Timeline(new KeyFrame(Duration.millis(1), event -> {
+            if (gameOver) {
+                displayGameOver(primaryStage);
+            }
+        }));
+        gameOverTimeline.setCycleCount(Timeline.INDEFINITE);
+        gameOverTimeline.play();
+    }
+    private void displayGameOver(Stage primaryStage) {
+        Text gameOverText = new Text("Game Over");
+        gameOverText.setFont(new Font(60));
+        gameOverText.setFill(Color.RED);
+        gameOverText.setX((WIDTH - gameOverText.getBoundsInLocal().getWidth()) / 2);
+        gameOverText.setY(HEIGHT / 2 - 100);
+
+        Text pressEnterToQuit = new Text("Press Escape to Quit");
+        pressEnterToQuit.setFont(new Font(25));
+        pressEnterToQuit.setFill(Color.BLUE);
+        pressEnterToQuit.setX(WIDTH / 2 - 110);
+        pressEnterToQuit.setY(HEIGHT /2 + 40);
+
+        Text pressSpaceToTryAgain = new Text("Press Space to try again");
+        pressSpaceToTryAgain.setFont(new Font(25));
+        pressSpaceToTryAgain.setFill(Color.BLUE);
+        pressSpaceToTryAgain.setX(WIDTH / 2 - 120);
+        pressSpaceToTryAgain.setY(HEIGHT /2 + 70);
+
+        root.getScene().setOnKeyPressed(keyEvent -> {
+            if(keyEvent.getCode() == KeyCode.ESCAPE){
+                primaryStage.close();
+            }
+            else if (keyEvent.getCode() == KeyCode.SPACE){
+                restartGame();
+                timeline.stop();
+                primaryStage.close();
+                start(new Stage());
+
+            }
+        });
+        root.getChildren().addAll(gameOverText, pressEnterToQuit, pressSpaceToTryAgain);
+    }
+    private void restartGame() {
+        gameOver = false;
+        ballSpeedX = -1.5;
+        ballSpeedY = -1.5;
+        root.getChildren().clear();
+        createPaddle();
+        createBall();
+        createBricks();
     }
 }
